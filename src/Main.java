216cd77061a29b6aa31e83bc225f1e5ba7c27ae1
tezkerek <andrei.ancuta@gmail.com>
2023-04-18 23:54:57 +0300

    Run menu from Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import menu.Menu;
+import repository.InMemoryRepository;
+import repository.Repository;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("PAOTIFY v0.1.0");
+
+        Repository repo = new InMemoryRepository();
+        var menu = new Menu(repo);
+        menu.run();
     }
 }
